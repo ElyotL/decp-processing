4696d3ab60bae77eec5156cefd8a8b3669bb7713
Colin Maudry <colin@maudry.com>
2025-04-04 17:02:55 +0200

    Refactor get pour que les retry ne soient que pour la tâche de téléchargement #29

--- src/tasks/get.py
+++ src/tasks/get.py
@@ -1,7 +1,12 @@
 import polars as pl
+
+# import pandas as pd
 from httpx import get
 import os
 import json
+
+# import pdb
+
 
 from polars.polars import ColumnNotFoundError
 from prefect import task
@@ -9,84 +14,28 @@ from pathlib import Path
 
 from tasks.output import save_to_files
 
+# from tasks.clean import preprocess_json_2022
+
 
 @task(retries=5, retry_delay_seconds=5)
-def get_json(url, file_name, date_now):
+def get_json(date_now, json_file: dict):
+    url = json_file["url"]
+    filename = json_file["file_name"]
+
     if url.startswith("https"):
         # Prod file
-        decp_json_file: Path = Path(f"data/{file_name}_{date_now}.json")
+        decp_json_file: Path = Path(f"data/{filename}_{date_now}.json")
     else:
         # Test file, pas de téléchargement
         decp_json_file: Path = Path(url)
-
     if not (os.path.exists(decp_json_file)):
         request = get(url, follow_redirects=True)
         with open(decp_json_file, "wb") as file:
             file.write(request.content)
     else:
-        print(
-            f"[{file_name.split('/')[-1]}] DECP d'aujourd'hui déjà téléchargées ({date_now})"
-        )
+        print(f"[{filename}] DECP d'aujourd'hui déjà téléchargées ({date_now})")
 
-    with open(decp_json_file) as f:
-        decp_json = json.load(f)
-
-    if file_name.split("/")[-1] == "decp-2022":
-        path = decp_json["marches"]
-    else:
-        path = decp_json["marches"]["marche"]
-
-    df: pl.DataFrame = pl.json_normalize(
-        path,
-        strict=False,
-        infer_schema_length=10000,
-    )
-
-    # Pour l'instant on ne garde pas les champs qui demandent une explosion
-    # ou une eval
-    # à part titulaires
-
-    columns_to_drop = [
-        # Pas encore incluses
-        "typesPrix.typePrix",
-        "considerationsEnvironnementales.considerationEnvironnementale",
-        "considerationsSociales.considerationSociale",
-        "techniques.technique",
-        "modalitesExecution.modaliteExecution",
-        "modifications",
-        "actesSousTraitance",
-        "modificationsActesSousTraitance",
-        # Champs de concessions
-        "_type",  # Marché ou Contrat de concession
-        "autoriteConcedante",
-        "concessionnaires",
-        "donneesExecution",
-        "valeurGlobale",
-        "montantSubventionPublique",
-        "dateSignature",
-        "dateDebutExecution",
-        # Champs ajoutés par e-marchespublics (decp-2022)
-        "offresRecues_source",
-        "marcheInnovant_source",
-        "attributionAvance_source",
-        "sousTraitanceDeclaree_source",
-        "dureeMois_source",
-    ]
-
-    for col in columns_to_drop:
-        try:
-            df = df.drop(col)
-        except ColumnNotFoundError:
-            pass
-
-    print(f"[{file_name}]", df.shape)
-
-    file = f"dist/get/{file_name}_{date_now}"
-    if not os.path.exists("dist/get"):
-        os.mkdir("dist/get")
-    save_to_files(df, file)
-
-    return file
+    return decp_json_file
 
 
 @task
@@ -95,7 +44,79 @@ def get_decp_json(json_files: dict, date_now: str) -> list:
     return_files = []
     for json_file in json_files:
         if json_file["process"] is True:
-            file = get_json(json_file["url"], json_file["file_name"], date_now)
+            decp_json_file: Path = get_json(date_now, json_file)
+
+            with open(decp_json_file) as f:
+                decp_json = json.load(f)
+
+            filename = json_file["file_name"]
+            # if filename == "decp-2022":
+            #     preprocessed = preprocess_json_2022(decp_json)
+            #     df_titulaires: pd.DataFrame = pd.json_normalize(preprocessed["titulaires_dict"], record_path="titulaires")
+            #     df_titulaires: pl.DataFrame = pl.from_pandas(df_titulaires)
+            #
+            #     df: pl.DataFrame = pl.json_normalize(preprocessed["marches_dict"]["marches"], strict=False, infer_schema_length=20000)
+            #
+            #     assert df.shape[0] == df_titulaires.shape[0]
+            #     pl.concat([df, df_titulaires], how="horizontal")
+            #     pdb.set_trace()
+            #     assert "titulaires" in df.columns
+
+            path = decp_json["marches"]["marche"]
+            df: pl.DataFrame = pl.json_normalize(
+                path,
+                strict=False,
+                infer_schema_length=10000,
+            )
+
+            df = df.with_columns(
+                pl.lit(f"data.gouv.fr {filename}.json").alias("source_open_data")
+            )
+
+            # Pour l'instant on ne garde pas les champs qui demandent une explosion
+            # ou une eval
+            # à part titulaires
+
+            columns_to_drop = [
+                # Pas encore incluses
+                "typesPrix.typePrix",
+                "considerationsEnvironnementales.considerationEnvironnementale",
+                "considerationsSociales.considerationSociale",
+                "techniques.technique",
+                "modalitesExecution.modaliteExecution",
+                "modifications",
+                "actesSousTraitance",
+                "modificationsActesSousTraitance",
+                # Champs de concessions
+                "_type",  # Marché ou Contrat de concession
+                "autoriteConcedante",
+                "concessionnaires",
+                "donneesExecution",
+                "valeurGlobale",
+                "montantSubventionPublique",
+                "dateSignature",
+                "dateDebutExecution",
+                # Champs ajoutés par e-marchespublics (decp-2022)
+                "offresRecues_source",
+                "marcheInnovant_source",
+                "attributionAvance_source",
+                "sousTraitanceDeclaree_source",
+                "dureeMois_source",
+            ]
+
+            for col in columns_to_drop:
+                try:
+                    df = df.drop(col)
+                except ColumnNotFoundError:
+                    pass
+
+            print(f"[{filename}]", df.shape)
+
+            file = f"dist/get/{filename}_{date_now}"
+            if not os.path.exists("dist/get"):
+                os.mkdir("dist/get")
+            save_to_files(df, file)
+
             return_files.append(file)
 
     return return_files
