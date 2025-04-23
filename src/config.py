7661c5716d92c2dedaa6b1c0afc2d0fa4d66a69c
Colin Maudry <colin@maudry.com>
2025-04-23 23:29:26 +0200

    Nouvelles env #27

--- src/config.py
+++ src/config.py
@@ -11,8 +11,12 @@ if not os.path.exists(".env"):
 load_dotenv()
 
 DATE_NOW = datetime.now().isoformat()[0:10]  # YYYY-MM-DD
+MONTH_NOW = DATE_NOW[2:10]
+
 DIST_DIR = f"dist/" + DATE_NOW
 DECP_PROCESSING_PUBLISH = os.getenv("DECP_PROCESSING_PUBLISH")
+
+SIRENE_DATA_DIR = os.getenv("SIRENE_DATA_DIR")
 
 with open(os.environ["DECP_JSON_FILES_PATH"]) as f:
     DECP_JSON_FILES = json.load(f)
