1580659eae7e80cc6a297c03afc9e18ffbf68912
Colin Maudry <colin@maudry.com>
2025-05-27 22:06:01 +0200

    Pas besoin d'un déploiement dédié pour les run one shot #37

--- src/deploy.py
+++ src/deploy.py
@@ -7,26 +7,11 @@ if __name__ == "__main__":
         source="https://github.com/ColinMaudry/decp-processing.git",
         entrypoint="flows.py:decp-processing",
     ).deploy(
-        name="decp-processing-daily",
+        name="decp-processing",
         description="Tous les jours du lundi au vendredi à 6h00",
         work_pool_name="local",
         ignore_warnings=True,
         cron="0 6 * * 1-5",
-        job_variables={
-            # Les variables d'environnement sont sont donc fixées au moment du déploiement,
-            # pas dans un .env au moment du run
-            "env": dotenv_values(".env")
-        },
-    )
-
-    flow.from_source(
-        source="https://github.com/ColinMaudry/decp-processing.git",
-        entrypoint="flows.py:decp-processing",
-    ).deploy(
-        name="decp-processing-once",
-        description="Exécution ponctuelle",
-        work_pool_name="local",
-        ignore_warnings=True,
         job_variables={
             # Les variables d'environnement sont sont donc fixées au moment du déploiement,
             # pas dans un .env au moment du run
